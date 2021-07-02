e2594778f7999c4f720ba6052fd001e255293f72
sleepearn-dev <86544360+sleepearn-dev@users.noreply.github.com>
2021-07-02 18:09:30 +0700

    Update kardia_pools.js

--- src/features/configure/vault/kardia_pools.js
+++ src/features/configure/vault/kardia_pools.js
@@ -1,28 +1,3 @@
 export const kardiaPools = [
-    {
-      id: 'kaidex-beco-kai',
-      logo: 'kardiachain/kai-beco.png',
-      name: 'BECO-KAI KLP',
-      token: 'BECO-KAI KLP',
-      tokenDescription: 'Becoswap',
-      tokenAddress: '0x1eBbF8080149FF07381AFd148bA0AF007f78cD3c',
-      tokenDecimals: 18,
-      earnedToken: 'pillowBecoBECO-KAI',
-      earnedTokenAddress: '0xEC58663962E03B1c209fe505921a98109c54eB9c',
-      earnContractAddress: '0xEC58663962E03B1c209fe505921a98109c54eB9c',
-      pricePerFullShare: 1,
-      tvl: 0,
-      oracle: 'lps',
-      oracleId: 'kaidex-beco-kai',
-      oraclePrice: 0,
-      depositsPaused: false,
-      status: 'active',
-      platform: 'SleepEarn.Finance',
-      assets: ['BECO'],
-      callFee: 0.5,
-      withdrawalFee: '0.05%',
-      buyTokenUrl: 'https://kaidex.io/exchange/0x1eBbF8080149FF07381AFd148bA0AF007f78cD3c',
-      addLiquidityUrl: 'https://kaidex.io/portfolio/add/0x1eBbF8080149FF07381AFd148bA0AF007f78cD3c',
-    }
-  ];
+];
   
